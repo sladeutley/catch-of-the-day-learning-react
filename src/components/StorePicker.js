0de238b680deb32bfec6c4734f18c22766e5aeb0
Slade Utley <slade.utley@gmail.com>
2018-08-02 15:54:06 -0500

    using jsx

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React from "react";
 
 // put a class as capitalized bc it indicates to us that it's a class and reusable
 // Also, every class in react needs at least one method inside it, called render, which determines what html elements do i render to the dom/page
+
 class StorePicker extends React.Component {
   render() {
-    return <p>Hello!</p>;
+    // *** could do below, kinda like document.createElement
+    // return React.createElement('p', { className: 'hey'}, 'Heyyyoooo')
+    return (
+      <form className="store-selector">
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store -></button>
+        {/* how do I make the side arrow */}
+      </form>
+    );
+
+    // *********** if have sibling tags (not sure exactly what that is?), like a p tag before the form tag, MUST put it in a <React.Fragment>, or import { Fragment } at top of page (e.g. import React, { Fragment } from "react";), then can just put <Fragment> -->
+    // return (
+    //   <React.Fragment>
+    //     <p>fish</p>
+    //     <form className="store-selector">
+    //       <h2>Please Enter Search</h2>
+    //     </form>
+    //   </React.Fragment>
+    // );
   }
 }
 
