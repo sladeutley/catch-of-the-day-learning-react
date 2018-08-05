cc80d54688976ef834cefb0a5154ddd037973ad3
Slade Utley <slade.utley@gmail.com>
2018-08-05 16:42:37 -0500

    created header, inventory, and order component

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 // import { Component } from 'react'; --> could deconstruct it this way and then dont have to put React.Component, doesn't matter
 // Also, above is ES6 way of doing same thing below, I think
 // const React = require('react');
-import { render } from 'react-dom';
-import StorePicker from './components/StorePicker'
+import { render } from "react-dom";
+import StorePicker from "./components/StorePicker";
+import App from "./components/App";
 // import css here instead of doing it in index.html. You can do either one though.
-import './css/style.css';
+import "./css/style.css";
 
 // render(<p>HEYYYYY</p>, document.querySelector('#main'));
 // could do above, but want to use the class StorePicker, so -->
-render(<StorePicker />, document.querySelector('#main'));
+render(<App />, document.querySelector("#main"));
 // *** Note, StorePicker has a self closing tag, its the same as <StorePicker></StorePicker>
-
