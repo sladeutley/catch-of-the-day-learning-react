f113cbdcede5839859e49ae0b7af27241b308814
Slade Utley <slade.utley@gmail.com>
2018-08-12 15:45:26 -0500

    loading sample fish data into state on click

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes";
 
 class App extends React.Component {
   // creating state
@@ -26,6 +27,9 @@ class App extends React.Component {
       fishes
     });
   };
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes })
+  };
   render() {
     return (
       <div className="catch-of-the-day">
@@ -33,7 +37,10 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
         {/* Above is passing the addFish function to Inventory, then to addFish component through PROPS!!!! */}
       </div>
     );
